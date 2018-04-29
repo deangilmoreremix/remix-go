72f0fcd188464b355f6c6da6f1495782e9fb6e63
Eugene Butusov <EvgenyButusov@gmail.com>
2018-04-29 19:45:29 +0300

    #653582524512686 Remix GO: Implement legacy login
    basic legacy login logic

--- config/env/all.js
+++ config/env/all.js
@@ -6,4 +6,12 @@ module.exports = {
     id: process.env.CLIENT_ID || 'default',
     secret: process.env.CLIENT_SECRET || 'default',
   },
+  loginServer: {
+    url: process.env.LOGIN_SERVER_URL || 'http://localhost:8888',
+    urlWithAuth: process.env.LOGIN_SERVER_URL_WITH_AUTH || 'http://testuser:password@localhost:8888'
+  },
+  secret: process.env.SECRET || 'dummy secret value',
+  appHostname: process.env.APP_HOSTNAME || 'http://localhost:3000',
+  cookieDomain: process.env.COOKIE_DOMAIN || '',
+  forceSsl: process.env.FORCE_SSL || false,
 };
--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const { port } = require('./config/config');
 const dev = process.env.NODE_ENV !== 'production';
 
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const next = require('next');
 const mobxReact = require('mobx-react');
 
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const checkAccess = require('./lib/express/check-access');
+
 mobxReact.useStaticRendering(true);
 
 app.prepare().then(() => {
   const server = express();
-  server.use(cookieParser());
-  server.get('*', (req, res) => {
+  require('./lib/express/webmaker-auth')(server);
+
+  server.get('*', checkAccess, (req, res) => {
     handle(req, res);
   });
   server.listen(port);
