12357b809a8a033e73f662125c94433d8a294a3a
Eugene Butusov <EvgenyButusov@gmail.com>
2018-04-30 23:38:24 +0300

    #655078383716784 Remix GO: Implement templates view
    improve files structure

---
+++ components/wizard/Templates.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+import { inject, observer } from 'mobx-react';
+
+@inject('store')
+@observer
+export default class Editor extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return <div>this is a template catalog</div>;
+  }
+}
---
+++ pages/edit.js
@@ -0,0 +1,4 @@
+import pageFactory from '../components/hoc/pageFactory';
+import Editor from '../components/wizard/Editor';
+
+export default pageFactory(Editor, 'editor');
