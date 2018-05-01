af4f9cd5a8e6d349f4131c356d951e6f63d90afe
Eugene Butusov <EvgenyButusov@gmail.com>
2018-05-02 00:30:00 +0300

    #655078383716784 Remix GO: Implement templates view
    setup footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,7 @@ import { Container } from 'reactstrap';
 export default () => (
   <Container>
     <footer className="footer">
+      <div className="copyright">© VideoRemix | View our terms of service</div>
     </footer>
   </Container>
 );
