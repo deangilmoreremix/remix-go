80dd1a42e93a329c9c2912a328132fdb66b3eccc
Eugene Butusov <EvgenyButusov@gmail.com>
2018-05-02 20:16:07 +0300

    #655078383716783 Remix GO: Integrate Popcorn.JS
    implement basic editor layout

--- components/wizard/Editor.js
+++ components/wizard/Editor.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import { Container, Col, Row } from 'reactstrap';
 import { inject, observer } from 'mobx-react';
 
 @inject('store')
@@ -7,11 +8,22 @@ export default class Editor extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
-    return <div>this is an editor</div>;
+    return (
+      <Fragment>
+        <Container fluid className="editor-wrapper">
+          <Row className="toolbar" style={{background: '#00ff00'}}>
+            <Col style={{textAlign: 'middle'}}>.col</Col>
+          </Row>
+          <Row className="canvas" style={{background: '#0000ff'}}>
+            <Col xs="2">.col</Col>
+            <Col style={{background: '#ff0000'}}>.col</Col>
+            <Col xs="2">.col</Col>
+          </Row>
+        </Container>
+      </Fragment>);
   }
 }
