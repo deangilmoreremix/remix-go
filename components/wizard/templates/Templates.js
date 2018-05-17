25f7775872f357f45fca27210896f891b858a8c6
Eugene Butusov <EvgenyButusov@gmail.com>
2018-05-17 15:26:32 +0300

    Fix correct route to templates endpoint

--- components/wizard/templates/Templates.js
+++ components/wizard/templates/Templates.js
@@ -54,7 +54,7 @@ export default class Templates extends Component {
   onSearch = async (query) => {
     const { api } = this.props;
     const { elements } = this.state;
-    const newElements = await api.list(elements.length, query);
+    const newElements = await api.templates(elements.length, query);
     this.setState({
       elements: newElements,
       hasMore: newElements.length > 0,
