114acfbdf93d1149240d96617da8f3e55a7dbfef
Eugene Butusov <EvgenyButusov@gmail.com>
2018-05-22 12:56:06 +0300

    #678072677931018 Remix GO: Implement Email Campaigns
    finalize layout for email campaign

--- components/wizard/publisher/campaigns/EmailCampaign.js
+++ components/wizard/publisher/campaigns/EmailCampaign.js
@@ -42,6 +42,57 @@ const EMBED_LOCATIONS = [
   },
 ];
 
+const EMAIL_PROVIDERS = [
+  {
+    key: 'aweber',
+    label: 'AWeber',
+    image: '../../../../static/images/publisher/email-campaign/aweber_hover.png',
+    paramsBuilder: () => {},
+  },
+  {
+    key: 'mailchimp',
+    label: 'MailChimp',
+    image: '../../../../static/images/publisher/email-campaign/mailchimp_hover.png',
+    paramsBuilder: () => {},
+  },
+  {
+    key: 'interspire',
+    label: 'Interspire',
+    image: '../../../../static/images/publisher/email-campaign/interspire_hover.png',
+    paramsBuilder: () => {},
+  },
+  {
+    key: 'getresponse',
+    label: 'GetResponse',
+    image: '../../../../static/images/publisher/email-campaign/gr_hover.png',
+    paramsBuilder: () => {},
+  },
+  {
+    key: 'infusionsoft',
+    label: 'InfusionSoft',
+    image: '../../../../static/images/publisher/email-campaign/infusionsoft_hover.png',
+    paramsBuilder: () => {},
+  },
+  {
+    key: 'sendlane',
+    label: 'Sendlane',
+    image: '../../../../static/images/publisher/email-campaign/sendlane_hover.png',
+    paramsBuilder: () => {},
+  },
+  {
+    key: 'constantcontact',
+    label: 'Constant Contact',
+    image: '../../../../static/images/publisher/email-campaign/constantcontact_hover.png',
+    paramsBuilder: () => {},
+  },
+  {
+    key: 'sendreach',
+    label: 'SendReach',
+    image: '../../../../static/images/publisher/email-campaign/sendreach_hover.png',
+    paramsBuilder: () => {},
+  },
+];
+
 export default class EmailCampaign extends Component {
   static propTypes = {
     className: PropTypes.string,
@@ -54,6 +105,9 @@ export default class EmailCampaign extends Component {
     embedLocation: EMBED_LOCATIONS[0],
     preload: true,
     autoplay: false,
+    embedPage: '',
+    emailProvider: null,
+    personalizedLink: '',
   };
 
   nextStage() {
@@ -82,7 +136,15 @@ export default class EmailCampaign extends Component {
 
   render() {
     const { className, project, onCampaignFinished } = this.props;
-    const { currentStage, embedLocation, preload, autoplay } = this.state;
+    const {
+      currentStage,
+      embedLocation,
+      preload,
+      autoplay,
+      embedPage,
+      emailProvider,
+      personalizedLink,
+    } = this.state;
 
     return (
       <Fragment>
@@ -156,8 +218,38 @@ export default class EmailCampaign extends Component {
               </div>
             </div>
             <div className={`embed-location ${currentStage.key !== 'embed-location' && 'hidden'}`}>
+              <h5 className="embed-title">URL Link to your page with your embedded video</h5>
+              <input
+                type="text"
+                className="embed-page-input"
+                value={embedPage}
+                onChange={({ target: { value } }) => this.setState({ embedPage: value })}
+              />
             </div>
             <div className={`service-provider ${currentStage.key !== 'service-provider' && 'hidden'}`}>
+              <ul className="service-provider-inner">
+                <li className="service-provider-step">
+                  <span>Select your Email Service Provider</span>
+                  <ul className="providers-list">
+                    {EMAIL_PROVIDERS.map((item, idx) => (
+                      <li
+                        className={`provider-item ${emailProvider && emailProvider.key === item.key && 'selected'}`}
+                        key={idx}
+                        onClick={() => this.setState({ emailProvider: item })}
+                      >
+                        <img src={item.image} alt={item.label} />
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+                <li className={`service-provider-step ${!emailProvider ? 'hidden' : ''}`}>
+                  <span>Copy & Paste this PersonalizedLink™ into your email campaign</span>
+                  <input className="personalized-link" type="text" value={personalizedLink} readOnly />
+                </li>
+                <li className={`service-provider-step ${!emailProvider ? 'hidden' : ''}`}>
+                  <span>Send your Personalized email campaign</span>
+                </li>
+              </ul>
             </div>
           </div>
           <div className="controls">
