fd64d025d00c80529d762c58fa556777a842eded
Eugene Butusov <EvgenyButusov@gmail.com>
2018-05-23 18:10:30 +0300

    #678072677931017 Remix GO: Implement Video Upload
    final style adjustments

--- components/common/VideoUpload.js
+++ components/common/VideoUpload.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { inject, observer } from 'mobx-react';
-import { Input, Progress } from 'reactstrap';
+import { Input, Progress, Alert } from 'reactstrap';
 import DropZone from 'react-dropzone';
 
 import PropTypes from '../../lib/PropTypes';
@@ -17,68 +17,96 @@ export default class VideoUpload extends Component {
     url: '',
     isUploading: false,
     uploadPercentage: 0,
+    error: null,
   };
 
   handleFileDrop = async (file) => {
     const { onVideoUploaded, api } = this.props;
     this.setState({ isUploading: true });
-    const response = await api.uploadMedia(
-      file,
-      progress => this.setState({ uploadPercentage: progress }),
-    );
-    this.setState({ isUploading: false });
-    onVideoUploaded(response.url);
+    try {
+      const response = await api.uploadMedia(
+        file,
+        progress => this.setState({ uploadPercentage: progress }),
+      );
+      this.setState({
+        uploadPercentage: 0,
+        isUploading: false,
+        error: null,
+      });
+      onVideoUploaded(response.url);
+    } catch (err) {
+      this.setState({
+        uploadPercentage: 0,
+        isUploading: false,
+        error: err.message,
+      });
+    }
   };
 
   retrieveVideoFromUrl = async () => {
+    this.setState({ uploadPercentage: 1, isUploading: true });
     const { onVideoUploaded } = this.props;
     const { url } = this.state;
     const videoTypeDetector = new MediaTypeDetector();
-    this.setState({ uploadPercentage: 1, isUploading: true });
     try {
+      this.setState({
+        uploadPercentage: 0,
+        isUploading: false,
+        error: null,
+      });
       onVideoUploaded((await videoTypeDetector.getMetadata(url)).source);
     } catch (err) {
-      this.setState({ uploadPercentage: 0, isUploading: false });
-      alert(err.message);
+      this.setState({
+        uploadPercentage: 0,
+        isUploading: false,
+        error: err.message,
+      });
     }
   };
 
   render() {
-    const { url, isUploading, uploadPercentage } = this.state;
+    const { url, isUploading, error, uploadPercentage } = this.state;
     return (
-      <div className="video-upload-box">
-        <DropZone
-          className={`upload-dropzone${isUploading ? ' hidden' : ''}`}
-          activeClassName="upload-dropzone hot"
-          onDrop={([file]) => this.handleFileDrop(file)}
-          accept="video/*"
-        >
-          <div className="dropzone-inner">
-            <img className="icon" src="../../static/images/upload.png" alt="Video upload" />
-            <h5 className="label">Click or drag your file here to start uploading it</h5>
+      <Fragment>
+        <div className="video-upload-box">
+          <DropZone
+            className={`upload-dropzone${isUploading ? ' hidden' : ''}`}
+            activeClassName="upload-dropzone hot"
+            onDrop={([file]) => this.handleFileDrop(file)}
+            accept="video/*"
+          >
+            <div className="dropzone-inner">
+              <img className="icon" src="../../static/images/upload.png" alt="Video upload" />
+              <h5 className="label">Click or drag your file here to start uploading it</h5>
+            </div>
+          </DropZone>
+          <div className={`upload-progress${!isUploading ? ' hidden' : ''}`}>
+            <h5 className="label">{uploadPercentage < 1 ? `${(uploadPercentage * 100).toFixed(0)}%` : 'Processing your media...'}</h5>
+            <Progress
+              animated
+              className="upload-progress-bar"
+              value={uploadPercentage * 100}
+            />
           </div>
-        </DropZone>
-        <div className={`upload-progress${!isUploading ? ' hidden' : ''}`}>
-          <h5 className="label">{uploadPercentage < 1 ? `${(uploadPercentage * 100).toFixed(0)}%` : 'Processing your media...'}</h5>
-          <Progress
-            animated
-            className="upload-progress-bar"
-            value={uploadPercentage * 100}
+          <h5 className={isUploading ? ' hidden' : ''}>or use link to external video hosting (YouTube, Vimeo, etc)</h5>
+          <Input
+            className={`external-video-link${isUploading ? ' hidden' : ''}`}
+            type="text"
+            value={url}
+            onChange={({ target: { value } }) => this.setState({ url: value })}
           />
+          <div className="external-video-submit-container">
+            <button
+              className={`go-button external-video-submit${isUploading ? ' hidden' : ''}`}
+              onClick={() => this.retrieveVideoFromUrl()}
+            >Get Video
+            </button>
+          </div>
+          <Alert className="alert-error" color="danger" isOpen={error} toggle={() => this.setState({ error: null })}>
+            {error}
+          </Alert>
         </div>
-        <h5 className={isUploading ? ' hidden' : ''}>or use link to external video hosting (YouTube, Vimeo, etc)</h5>
-        <Input
-          className={`external-video-link${isUploading ? ' hidden' : ''}`}
-          type="text"
-          value={url}
-          onChange={({ target: { value } }) => this.setState({ url: value })}
-        />
-        <button
-          className={`go-button external-video-submit${isUploading ? ' hidden' : ''}`}
-          onClick={() => this.retrieveVideoFromUrl()}
-        >Get Video
-        </button>
-      </div>
+      </Fragment>
     );
   }
 }
