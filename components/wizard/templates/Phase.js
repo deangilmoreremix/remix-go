5023181a279ca37f7db3e0094e1230194e0fb2f5
Roland Barro <rolandbarro@gmail.com>
2018-05-23 21:33:04 +0800

    656096750990781: Remix GO: Implement phase component
    Initial functionality added

--- components/wizard/templates/Phase.js
+++ components/wizard/templates/Phase.js
@@ -5,59 +5,90 @@ export default class Phase extends Component {
     super();
 
     this.state = {
-      phaseTitle: `Choose Template`,
-      phaseTabName: `chooseTemplate`,
-    }
+      title: `Choose Template`,
+      tabName: `chooseTemplate`,
+    };
 
     this.phaseTabs = {
-      ChooseTemplate: {
+      chooseTemplate: {
         phaseTitle: 'Choose Template',
         phaseTabName: `chooseTemplate`,
         phaseTabValue: 1,
+        selected: true,
       },
-      CustomizeVideo: {
+      customizeVideo: {
         phaseTitle: 'Customize Video',
         phaseTabName: `customizeVideo`,
         phaseTabValue: 2,
+        selected: false,
       },
-      PublishVideo: {
+      publishVideo: {
         phaseTitle: 'Publish & Video',
-        phaseTabName: `publishVideoo`,
+        phaseTabName: `publishVideo`,
         phaseTabValue: 3,
+        selected: false,
       },
-    }
+    };
+
+    this.TABS = {
+      CHOOSE_TEMPLATE: 1,
+      CUSTOMIZE_VIDEO: 2,
+      PUBLISH_VIDEO: 3,
+    };
   }
 
   render() {
+    const { title } = this.state;
+
+    const PhaseTab = (props) => {
+      const { handleClick } = props;
+      const { phaseTitle, phaseTabName, phaseTabValue, selected } = props.value;
+      return (
+        <div className={`stepper-tab-group ${(selected?'active':'')}`} >
+          <div className="gapped">
+            <div className="phase">
+              <div className="phase-label" onClick={() => onSelectPhase(phaseTabName)}>{phaseTabValue}</div>
+            </div>
+          </div>
+          <div className="stepper-tab-label">{phaseTitle}</div>
+        </div>
+      );
+    };
+
+    const deselectPhase = () => {
+      const { tabName } = this.state;
+      this.phaseTabs[tabName].selected = false;
+    };
+
+    const selectPhase = (tabName) => {
+      this.phaseTabs[tabName].selected = true;
+    };
+
+    const onSelectPhase = (tabName) => {
+      deselectPhase();
+      selectPhase(tabName);
+
+      const tabSelected = this.phaseTabs[tabName];
+      const { phaseTitle, phaseTabName } = tabSelected;
+      this.setState({
+        title: phaseTitle,
+        tabName: phaseTabName,
+      });
+    };
+
     return (
       <div className="phase-component">
-        {/* <PhaseTitle title={this.state} />
-        <PhaseTabs />
-        <PhaseContainer /> */}
         <div className="phase-state-title">
-          <div>Choose Template</div>
+          <div>{title}</div>
         </div>
         <div className="phase-state-tabs">
           <div className="stepper">
             <div className="line"></div>
-            <div className="gapped">
-              <div className="phase">
-                <div className="phase-label active">1</div>
-              </div>
-            </div>
-            <div className="gapped">
-              <div className="phase">
-                <div className="phase-label">2</div>
-              </div>
-            </div>
-            <div className="gapped">
-              <div className="phase">
-                <div className="phase-label">3</div>
-              </div>
-            </div>
+            <PhaseTab handleClick={onSelectPhase} value={this.phaseTabs.chooseTemplate} />
+            <PhaseTab handleClick={onSelectPhase} value={this.phaseTabs.customizeVideo} />
+            <PhaseTab handleClick={onSelectPhase} value={this.phaseTabs.publishVideo} />
           </div>
         </div>
-        <div className="phase-state-container">Tab Container</div>
       </div>
     );
   }
