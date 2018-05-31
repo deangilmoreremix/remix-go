f4c9392afa8431a1400913059c7e1b3fa62c4f97
Eugene Butusov <EvgenyButusov@gmail.com>
2018-06-01 01:26:02 +0300

    #688116496688092 Remix GO: Video Trimming missed
    temporary fixation

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.prepare().then(() => {
   });
   require('./lib/express/webmaker-auth')(server);
   server.use(express.json());
-  server.use(express.urlencoded());
+  server.use(express.urlencoded({ extended: true }));
   // server.post('/api/media/join', join);
   server.put('/api/media', processForm, isValidMedia, mediaUpload);
   server.get('/api/get-content-type', getContentType);
