8b5d34a759fa2a3b373fd6e1777d48bd392e767d
Eugene Butusov <EvgenyButusov@gmail.com>
2018-06-03 14:57:39 +0300

    Add loader.io config file

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.prepare().then(() => {
   require('./lib/express/webmaker-auth')(server);
   server.use(express.json());
   server.use(express.urlencoded({ extended: true }));
+  require('./lib/express/loaderio')(server);
   // server.post('/api/media/join', join);
   server.put('/api/media', processForm, isValidMedia, mediaUpload);
   server.get('/api/get-content-type', getContentType);
