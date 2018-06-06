d574445e6510bc2dbee164f2e2dcd8506891bc60
Roland Barro <rolandbarro@gmail.com>
2018-06-07 01:11:36 +0800

    689282016129799 [Defect] When user uses a project then loading sign does not get appear
    Added loading effects on Video and Audio tab on ‘use’ loading event

--- components/wizard/editor/WorkspaceContainer.js
+++ components/wizard/editor/WorkspaceContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 
 import PropTypes from '../../../lib/PropTypes';
+import Waiter from '../../common/Waiter';
 import EditorStateManager from '../../../lib/editor/editorStateManager';
 import VideoSelectionWorkspace from './workspaces/VideoSelectionWorkspace';
 import AudioSelectionWorkspace from './workspaces/AudioSelectionWorkspace';
@@ -14,7 +15,12 @@ export default class EditorStageChanger extends Component {
     className: PropTypes.string,
   };
 
+  state = {
+    waiter: null,
+  };
+
   render() {
+    const { waiter } = this.state;
     const {
       className,
       store: {
@@ -32,9 +38,11 @@ export default class EditorStageChanger extends Component {
             case EditorStateManager.STAGE_TYPES.VIDEO_CUSTOMISE:
               return (
                 <div className="scrollable full-height">
+                  { waiter ? <Waiter message={waiter.message} /> : null }
                   <VideoSelectionWorkspace
                     inWindow
                     onVideoSelected={async (video) => {
+                      this.setState({ waiter: { message: 'Loading video' } });
                       await activeProject.updateVideo(video);
                       activeProject.version = Math.random();
                       editorStateManager.stage = EditorStateManager.STAGE_TYPES.CAPTION_CUSTOMISE;
@@ -45,9 +53,11 @@ export default class EditorStageChanger extends Component {
             case EditorStateManager.STAGE_TYPES.AUDIO_CUSTOMISE:
               return (
                 <div className="scrollable full-height">
+                  { waiter ? <Waiter message={waiter.message} /> : null }
                   <AudioSelectionWorkspace
                     inWindow
                     onAudioSelected={async (audio) => {
+                      this.setState({ waiter: { message: 'Loading audio' } });
                       await activeProject.updateAudio(audio);
                       activeProject.version = Math.random();
                       editorStateManager.stage = EditorStateManager.STAGE_TYPES.CAPTION_CUSTOMISE;
