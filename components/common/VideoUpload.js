1027c29411d528eb8d14370439bc50e3a9701abb
Eugene Butusov <EvgenyButusov@gmail.com>
2018-06-07 16:55:49 +0300

    #701548124038524 [Defect] Wrong error message is appearing for .webm file format

--- components/common/VideoUpload.js
+++ components/common/VideoUpload.js
@@ -145,10 +145,10 @@ export default class VideoUpload extends Component {
                   if (file) {
                     return this.handleFileDrop(file);
                   } else if (noFile) {
-                    return this.setState({ error: 'This media format is not supported. Please try to upload MP4 video file.' });
+                    return this.setState({ error: 'This media format is not supported. Please try to upload MP4 or WebM video file.' });
                   }
                 }}
-                accept="video/*"
+                accept={['video/mp4', 'video/webm']}
               >
                 <div className="dropzone-inner">
                   <img className="icon" src="../../static/images/upload.png" alt="Video upload" />
