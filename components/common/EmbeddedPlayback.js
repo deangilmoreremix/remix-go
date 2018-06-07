82caf941ac4f881ea27566f96222d2c257fc62b0
Eugene Butusov <EvgenyButusov@gmail.com>
2018-06-07 18:16:07 +0300

    #700982048338897 Remix GO: Replace default posterframe

--- components/common/EmbeddedPlayback.js
+++ components/common/EmbeddedPlayback.js
@@ -38,7 +38,7 @@ export default class EmbeddedPlayback extends Component {
         showExtendedEndroll: false,
         showShare: false,
         allowFacebook: false,
-        thumbnailSrc: source.thumbnail,
+        thumbnail: source.thumbnail,
         data: JSON.stringify(source.popcornObject),
       },
     }, this.frameConductor.src);
