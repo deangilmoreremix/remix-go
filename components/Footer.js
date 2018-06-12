8779ced4a7519b21a6fece17b6c724ef74aca0fe
Eugene Butusov <EvgenyButusov@gmail.com>
2018-06-13 01:16:58 +0300

    #708433668298797 Remix GO: Add Changelog

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,14 @@ import { Container } from 'reactstrap';
 export default () => (
   <Container>
     <footer className="footer">
-      <div className="copyright">© VideoRemix | <a className="direct-link" href="http://dashboard.vidcloud.io/terms-of-service/">View our terms of service</a></div>
+      <div className="copyright">© VideoRemix |
+        <a className="direct-link" href="http://dashboard.vidcloud.io/terms-of-service/">
+          View our terms of service
+        </a> |
+        <a className="direct-link" href="//projects.videoremix.io/changelog">
+          Changelog
+        </a>
+      </div>
     </footer>
   </Container>
 );
