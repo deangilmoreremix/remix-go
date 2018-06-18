38618a4deaae4c73aa62447f564b77db0acff9f4
Eugene Butusov <EvgenyButusov@gmail.com>
2018-06-19 01:35:32 +0300

    #714823538684914 [Defect] Editor layout is resizing randomly
    rollback  checkpoint list layout change

--- components/wizard/editor/workspaces/construction/CheckpointsList.js
+++ components/wizard/editor/workspaces/construction/CheckpointsList.js
@@ -17,15 +17,17 @@ export default class CheckpointsList extends Component {
   render() {
     const { className, checkpoints, onCheckpointSelect } = this.props;
     return (
-      <div className={`thumbnail-canvas full-height full-width ${className || ''}`}>
-        <div className="thumbnail-canvas-scroll">
-          {checkpoints.map((item, idx) => (
-            <div key={idx} className="thumbnail-wrapper" onClick={() => onCheckpointSelect(item)}>
-              <Checkpoint at={item} />
-            </div>
-          ))}
+      <Container className={`full-height full-width ${className || ''}`}>
+        <div className="thumbnail-canvas">
+          <div className="thumbnail-canvas-scroll">
+            {checkpoints.map((item, idx) => (
+              <div key={idx} className="thumbnail-wrapper" onClick={() => onCheckpointSelect(item)}>
+                <Checkpoint at={item} />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      </Container>
     );
   }
 }
