977826a8d5946179544ebbe75dae96d2122186c0
Eugene Butusov <EvgenyButusov@gmail.com>
2018-06-25 21:00:33 +0300

    #720798676431313 Error on sharing GO video to Facebook Page

--- components/wizard/publisher/campaigns/SocialCampaign.js
+++ components/wizard/publisher/campaigns/SocialCampaign.js
@@ -12,7 +12,7 @@ const FB_APP_ID = '1728968890675795';
 const FACEBOOK_PERMISSIONS = 'manage_pages,pages_show_list';
 const FB_DEFAULT_USERPIC = 'http://emblemsbf.com/img/11864.jpg';
 const BACKEND_URL = 'https://api.videoremix.io';
-const MIN_FANS_PAGE = 2000;
+const MIN_FANS_PAGE = 0;
 
 const EMBED_LOCATIONS = [
   {
