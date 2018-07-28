98f3496239c06e2774f48cfbbc0a454287ae7467
Roland Barro <rolandbarro@gmail.com>
2018-07-29 00:24:34 +0800

    #758992637730808 Add support to display audio artworks/thumbnails on tiles
    Work in progress - backup

--- components/wizard/editor/workspaces/gridItems/AudioGridItem.js
+++ components/wizard/editor/workspaces/gridItems/AudioGridItem.js
@@ -4,12 +4,16 @@ import PropTypes from '../../../../../lib/PropTypes';
 
 const AudioGridItem = (props) => {
   const { onPreview, onUse, artwork, title, url } = props;
+  const testAW = 'https://cdn.vidcloud.io/resources/go/audios/laughing-female-friends-with-ukuleles_23-2147829691.jpg';
   return (
-    <div className="card" style={{ backgroundImage: `url(${artwork || '/static/images/editor/default-artwork.png'})` }}>
+    <div className="card" style={{ backgroundImage: `url(${testAW || '/static/images/editor/default-artwork.png'})` }}>
       <div className="overlay">
-        <div className="buttons-container">
-          <a className="button" onClick={() => onPreview(title, url)}>preview</a>
-          <a className="button button-primary" onClick={() => onUse(url)}>use</a>
+        <div className="buttons-container preview">
+          <a className="button btn-preview" onClick={() => onPreview(title, url)}>
+            <i className="fa fa-play"></i>
+          </a>
+          <p>{title}</p>
+          <a className="button button-primary btn-use" onClick={() => onUse(url)}>use</a>
         </div>
       </div>
     </div>
