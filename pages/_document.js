d3c2e660a387359837c51d9c19c63391dcf7e79a
Eugene Butusov <EvgenyButusov@gmail.com>
2018-08-19 01:25:30 +0300

    #769080983950912 [Defect] On page title, project name should appear in the Editor
    revert unexpected changes

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Main, NextScript } from 'next/document';
 import stylesheet from 'styles/index.scss';
-import { inject } from 'mobx-react';
 
-@inject('store')
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const {
@@ -15,19 +13,17 @@ export default class MyDocument extends Document {
   }
 
   render() {
-    const { customValue, store: { activeProject } } = this.props;
-
     return (
       <html lang="en">
-        <Head>
-          <title>{activeProject ? `${activeProject.name} - VideoRemix GO` : 'VideoRemix GO'}</title>
+        <head>
+          <title>VideoRemix GO</title>
           <link rel="shortcut icon" href="//cdn.vidcloud.io/resources/go/favicon.png" />
-        </Head>
+        </head>
         <body>
           {/* eslint-disable react/no-danger */}
           <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
           {/* eslint-enable react/no-danger */}
-          {customValue}
+          {this.props.customValue}
           <Main />
           <NextScript />
         </body>
