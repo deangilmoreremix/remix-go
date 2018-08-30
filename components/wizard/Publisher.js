d5dd34936fd15d01bd60406ba56c527a9e6a97b9
Eugene Butusov <EvgenyButusov@gmail.com>
2018-08-30 18:14:35 +0300

    Disable Opt-In Campaign for a while

--- components/wizard/Publisher.js
+++ components/wizard/Publisher.js
@@ -218,33 +218,33 @@ export default class Publisher extends Component {
                   >
                     Facebook
                   </button>
-                  <button
-                    className={`go-button action-button ${(currentUser.features[features.retarget] && currentUser.features[features.retarget].state === 'enabled') ? '' : 'inactive'}`}
-                    title={(currentUser.features[features.retarget] && currentUser.features[features.retarget].state === 'enabled') ? '' : 'This feature is not available on your type of subscription. Click here to details.'}
-                    onClick={() => {
-                      if (currentUser.features[features.retarget] && currentUser.features[features.retarget].state === 'enabled') {
-                        PopupboxManager.open({
-                          content: <RetargetCampaign
-                            className="campaign"
-                            project={activeProject}
-                            onCampaignFinished={() => PopupboxManager.close()}
-                          />,
-                          config: {
-                            titleBar: {
-                              enable: true,
-                              text: 'Opt-In/Retarget',
-                            },
-                            fadeIn: true,
-                            fadeInSpeed: 200,
-                          },
-                        });
-                      } else if (currentUser.features[features.retarget].link) {
-                        window.open(currentUser.features[features.retarget].link, '_blank');
-                      }
-                    }}
-                  >
-                    Opt-In/Retarget
-                  </button>
+                  {/*<button*/}
+                    {/*className={`go-button action-button ${(currentUser.features[features.retarget] && currentUser.features[features.retarget].state === 'enabled') ? '' : 'inactive'}`}*/}
+                    {/*title={(currentUser.features[features.retarget] && currentUser.features[features.retarget].state === 'enabled') ? '' : 'This feature is not available on your type of subscription. Click here to details.'}*/}
+                    {/*onClick={() => {*/}
+                      {/*if (currentUser.features[features.retarget] && currentUser.features[features.retarget].state === 'enabled') {*/}
+                        {/*PopupboxManager.open({*/}
+                          {/*content: <RetargetCampaign*/}
+                            {/*className="campaign"*/}
+                            {/*project={activeProject}*/}
+                            {/*onCampaignFinished={() => PopupboxManager.close()}*/}
+                          {/*/>,*/}
+                          {/*config: {*/}
+                            {/*titleBar: {*/}
+                              {/*enable: true,*/}
+                              {/*text: 'Opt-In/Retarget',*/}
+                            {/*},*/}
+                            {/*fadeIn: true,*/}
+                            {/*fadeInSpeed: 200,*/}
+                          {/*},*/}
+                        {/*});*/}
+                      {/*} else if (currentUser.features[features.retarget].link) {*/}
+                        {/*window.open(currentUser.features[features.retarget].link, '_blank');*/}
+                      {/*}*/}
+                    {/*}}*/}
+                  {/*>*/}
+                    {/*Opt-In/Retarget*/}
+                  {/*</button>*/}
                 </ActionsPane>
               </Col>
             </Row>
