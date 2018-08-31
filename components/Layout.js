69f91470e097ef1409347061d7556556cfe9bc75
Eugene Butusov <EvgenyButusov@gmail.com>
2018-08-31 10:56:44 +0300

    #801975120052912 Add Intercom widget to Go editor

--- components/Layout.js
+++ components/Layout.js
@@ -34,18 +34,18 @@ class Layout extends Component {
           <Header />
           <Container {...this.props} className="main">
             {this.props.children}
+            {this.store.currentUser ?
+              <Intercom
+                appID={this.store.common.intercom.appId}
+                user={{
+                  email: this.store.currentUser.email,
+                  fullName: this.store.currentUser.fullName,
+                  hash: this.store.currentUser.hash,
+                  createdAt: Math.floor(Date.parse(this.store.currentUser.createdAt) / 1000),
+                }}
+                domain="videoremix.io"
+              /> : null}
           </Container>
-          {this.store.currentUser ?
-            <Intercom
-              appID={this.store.common.intercom.appId}
-              user={{
-                email: this.store.currentUser.email,
-                fullName: this.store.currentUser.fullName,
-                hash: this.store.currentUser.hash,
-                createdAt: Math.floor(Date.parse(this.store.currentUser.createdAt) / 1000),
-              }}
-              domain="videoremix.io"
-            /> : null}
           <Footer />
         </div>
       </Provider>
