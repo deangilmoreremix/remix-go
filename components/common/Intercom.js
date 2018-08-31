45b67447dce2eeb313855e748af2d5f6f9cf936e
Eugene Butusov <EvgenyButusov@gmail.com>
2018-08-31 11:08:08 +0300

    #801975120052912 Add Intercom widget to Go editor

--- components/common/Intercom.js
+++ components/common/Intercom.js
@@ -113,13 +113,13 @@ export default class Intercom extends Component {
     return false;
   }
 
-  componentWillUnmount() {
-    if (!canUseDOM || !window.Intercom) return false;
-
-    window.Intercom('shutdown');
-
-    delete window.Intercom;
-  }
+  // componentWillUnmount() {
+  //   if (!canUseDOM || !window.Intercom) return false;
+  //
+  //   window.Intercom('shutdown');
+  //
+  //   delete window.Intercom;
+  // }
 
   render() {
     return false;
