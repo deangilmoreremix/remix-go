d72c76fbf0119c256e6747bdf9b650f02517b05a
Eugene Butusov <EvgenyButusov@gmail.com>
2018-11-12 10:30:29 +0300

    #862207274637338 Implement LinkedIn Personalized Data Provider
    prepare for rollout

--- components/wizard/Publisher.js
+++ components/wizard/Publisher.js
@@ -123,7 +123,7 @@ export default class Publisher extends Component {
         /> : null }
         <iframe
           title="Facebook conductor"
-          src="//dev-cdn.vidcloud.io/social-campaign/social-campaign.html"
+          src="//cdn.vidcloud.io/social-campaign/social-campaign.html"
           frameBorder="0"
           className="conductor-iframe"
           id="conductor-iframe"
