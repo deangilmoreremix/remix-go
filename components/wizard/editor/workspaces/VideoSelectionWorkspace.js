d0f4412daa30f26606f079fcab763ab5bc04d5a9
aleksey leichenko <alekseyleychenko@gmail.com>
2019-10-16 13:24:00 +0300

    #1137470388275060 - Remix GO: A library should be maintained, if users upload any video from their system through "Import Your Own Videos"

--- components/wizard/editor/workspaces/VideoSelectionWorkspace.js
+++ components/wizard/editor/workspaces/VideoSelectionWorkspace.js
@@ -91,9 +91,6 @@ export default class VideoSelectionWorkspace extends Component {
 
   onScopeChange = async (scope) => {
     if (scope !== this.state.scope) {
-      console.log('state', this.state);
-      console.log('scope', scope);
-      console.log('this.state.scope', this.state.scope);
       this.setState({
         scope,
         [scope]: {
@@ -129,7 +126,6 @@ export default class VideoSelectionWorkspace extends Component {
     const { api, className, inWindow = false, onVideoSelected } = this.props;
     const { scope } = this.state;
     const { hasMore, elements } = this.state[scope];
-    console.log('arr of elements', elements);
     const editable = (scope === api.constructor.ASSET_SCOPES.UPLOADS);
 
     const sizes = inWindow ?
@@ -188,8 +184,7 @@ export default class VideoSelectionWorkspace extends Component {
                     value={item.title}
                     onSave={this.onRename(item)}
                   />}
-              </div>
-            ))}
+              </div>))}
         </VideoGallery>
       </Fragment>
     );
