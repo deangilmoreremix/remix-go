fa0f309e092fd69dcc8c275621e71c7a3f823f4c
truespun <truespun@gmail.com>
2020-08-21 17:04:09 +0300

    #fix/linkedin-social-campaign

--- components/wizard/Publisher.js
+++ components/wizard/Publisher.js
@@ -130,7 +130,7 @@ export default class Publisher extends Component {
         ) : null }
         <iframe
           title="Iframe social conductor"
-          src={`${cdnHostname}/social-campaign/social-campaign.html`}
+          src={'https://dev-cdn.videoremix.io/social-campaign/social-campaign.html'}
           frameBorder="0"
           className="conductor-iframe"
           id="conductor-iframe"
