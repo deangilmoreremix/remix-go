c29efcf53922ddc2410204ae5deb382575603926
Akande Toluwalase Elias <akande.tolu@yahoo.com>
2022-01-28 13:39:16 +0100

    change s3 key

--- config/env/all.js
+++ config/env/all.js
@@ -70,7 +70,7 @@ module.exports = {
     cdnSocialWeb: process.env.CDN_SOCIAL_HOSTNAME || 'https://cdn.vidcloud.io',
     mediaCdn: process.env.MEDIA_CDN_HOSTNAME || '',
     streamingCdn: process.env.STREAMING_CDN_HOSTNAME || '',
-    key: process.env.S3_KEY || 'AKIAIAQ6WDZIWRHJTGVA',
+    key: process.env.S3_KEY || 'AKIAZIC4SYEXDCIH4FI6',
     bucket: process.env.S3_BUCKET || 'videoremix',
     mediaBucket: process.env.S3_MEDIA_BUCKET || '',
     secret: process.env.S3_SECRET || 'rVsDp2sM1AyaebUqY3WY9vDefDIE/s6WbqePUVYz',
